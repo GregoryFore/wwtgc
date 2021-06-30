f649e004a34cc9faf076b73eaf4c5fd4a993937f
Greg Foreman <gregforeman@Gregs-MacBook-Pro.local>
2021-06-30 11:51:27 -0700

    style navigtion bar

--- js/wedding-home.js
+++ js/wedding-home.js
@@ -6,5 +6,15 @@ function scrollNavBar() {
     } else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
         document.getElementById("navbar").classList.remove("sticky")
     }
+}
 
+
+
+function openNav() {
+    document.getElementById("overlayNav").style.width = "100%";
+    console.log('open');
+}
+
+function closeNav() {
+    document.getElementById("overlayNav").style.width = "0%";
 }
